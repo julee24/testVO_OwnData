5aada519f4a2d25037e26e8bfe342f011ade5506
julee24 <77472171+julee24@users.noreply.github.com>
2021-09-03 20:07:15 +0900

    Resize your dataset images

---
+++ data_preparation/resize_image.py
@@ -0,0 +1,15 @@
+# for resizing all the images in selected file
+import PIL
+import os
+import os.path
+from PIL import Image
+
+# change the directory accordingly
+f = r'/home/jaun/Downloads/SC-SfMLearner-Release-master/Dataset/kitti_odom_test/sequences/11/image_2'
+
+for file in os.listdir(f):
+    f_img = f + "/" + file
+    img = Image.open(f_img)
+    # resizes image to KITTI dataset size
+    img = img.resize((1226,370))
+    img.save(f_img)
