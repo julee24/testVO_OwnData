9e356520a3fe2deaf3a5c5032e937ab4c6f0420e
julee24 <77472171+julee24@users.noreply.github.com>
2021-09-28 09:36:10 +0900

    Update resize_image.py

--- data_preparation/resize_image.py
+++ data_preparation/resize_image.py
@@ -10,6 +10,7 @@ f = r'/home/jaun/Downloads/SC-SfMLearner-Release-master/Dataset/kitti_odom_test/
 for file in os.listdir(f):
     f_img = f + "/" + file
     img = Image.open(f_img)
+    
     # resizes image to KITTI dataset size
     img = img.resize((1226,370))
     img.save(f_img)
